2845fa6a9fb2948c71d20225176c001e44af6ef5
Sods2 <Sods2mail@gmail.com>
2020-06-28 15:16:44 +0100

    preparation to submission

--- src/com/RenfrewshireSoftFruitsCooperative_Project/Main.java
+++ src/com/RenfrewshireSoftFruitsCooperative_Project/Main.java
@@ -2,6 +2,9 @@ package com.RenfrewshireSoftFruitsCooperative_Project;
 
 import com.RenfrewshireSoftFruitsCooperative_Project.java.Console.Console;
 
+/**
+ * @author Alessandro Spano (Student N. rmb19196)
+ */
 public class Main {
 
     public static void main(String[] args) {
